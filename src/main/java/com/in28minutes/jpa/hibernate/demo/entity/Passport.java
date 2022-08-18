a470de693a51ca0e74f121fa955e728209db70b1
buentellopc <buentellopc@gmail.com>
2022-08-18 11:10:49 -0500

    [step28] bidirectional mapping

--- src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Passport.java
+++ src/main/java/com/in28minutes/jpa/hibernate/demo/entity/Passport.java
@@ -1,9 +1,6 @@
 package com.in28minutes.jpa.hibernate.demo.entity;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
 
 @Entity
 public class Passport {
@@ -14,6 +11,9 @@ public class Passport {
 
 	@Column(nullable = false)
 	private String number;
+
+	@OneToOne(fetch = FetchType.LAZY, mappedBy = "passport")
+	Student student;
 
 	protected Passport() {
 	}
@@ -32,6 +32,15 @@ public class Passport {
 
 	public Long getId() {
 		return id;
+	}
+
+
+	public Student getStudent() {
+		return student;
+	}
+
+	public void setStudent(Student student) {
+		this.student = student;
 	}
 
 	@Override
